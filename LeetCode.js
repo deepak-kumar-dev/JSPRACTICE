e101aea1463e304b8e19bfa207d29953e78f4fca
Deepak <deepakkumarlamoria@gmail.com>
2024-02-10 20:20:12 +0530

    Day 4 :  Allow One Function Call

--- LeetCode.js
+++ LeetCode.js
@@ -114,3 +114,31 @@ var argumentsLength = function(...args) {
 /**
  * argumentsLength(1, 2, 3); // 3
  */
+
+//---------------------------------------------------
+
+/**
+ * Day 4 : 2666. Allow One Function Call
+ * 
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    var check = 0;
+    return function(...args){
+        if(check){
+            return;
+        }else{
+        ++check;
+        return fn(...args)
+        }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */

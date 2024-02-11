f32b0c63e84698a1c81542b70d0f0b2c84fe42b7
Deepak <deepakkumarlamoria@gmail.com>
2024-02-11 20:55:40 +0530

    Day 5 - Apply Transform Over Each Element in Array

--- LeetCode.js
+++ LeetCode.js
@@ -166,3 +166,21 @@ var isEmpty = function(obj) {
         }
     }
 };
+
+//---------------------------------------------------------
+
+
+/**
+ * Day 5 : 2635. Apply Transform Over Each Element in Array
+ * 
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    var returnedArray = []
+    for(i=0; i<arr.length; i++){
+        returnedArray.push(fn(arr[i], i))
+    }
+    return returnedArray;
+};

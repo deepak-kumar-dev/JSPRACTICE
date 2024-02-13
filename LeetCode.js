183d97b529dbb03ad42225b070c56b523c6ffbf5
Deepak <deepakkumarlamoria@gmail.com>
2024-02-13 22:54:59 +0530

    Day 6:  Calculator with Method Chaining

--- LeetCode.js
+++ LeetCode.js
@@ -184,3 +184,55 @@ var map = function(arr, fn) {
     }
     return returnedArray;
 };
+
+//--------------------------------------------------
+
+
+    /** 
+     * Day 6: 2726. Calculator with Method Chaining
+     */
+class Calculator {
+    constructor(value) {
+        this.val = value;
+    }
+
+    add(value){
+        this.val = this.val + value;
+        return this;
+    }
+
+    subtract(value){
+        this.val = this.val - value;
+        return this;
+    }
+  
+    multiply(value) {
+        this.val = this.val * value;
+        return this;
+    }
+    
+    divide(value) {
+        if(value === 0) throw new Error("Division by zero is not allowed"); 
+        this.val = this.val / value;
+        return this;
+    }
+    
+    power(value) {
+        this.val = Math.pow(this.val,value);
+        return this;
+    }
+    
+    getResult() {
+        return this.val;
+    }
+}
+
+// Input: 
+// actions = ["Calculator", "add", "subtract", "getResult"], 
+// values = [10, 5, 7]
+// Output: 8
+// Explanation: 
+// new Calculator(10).add(5).subtract(7).getResult() // 10 + 5 - 7 = 8
+
+
+///---------------------------------------------------------------------

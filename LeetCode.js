393ccc19c8004529e9545f3af3f14b195fcae16d
Deepak <deepakkumarlamoria@gmail.com>
2024-02-19 21:14:46 +0530

    Day 9 - Function Composition

--- LeetCode.js
+++ LeetCode.js
@@ -276,3 +276,31 @@ for (let i = 0; i < arr.length; i += size) {
 
 return chunkedArray;
 };
+
+//-----------------------------------------------------
+
+
+/**
+ * 
+ * 
+ * Day 9 : 2629. Function Composition
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    
+    return function(x) {
+        let a = x;
+        for(let i = functions.length-1; i >= 0; i--){
+            a = functions[i](a);
+        }
+        return a;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
+
+

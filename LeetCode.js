976622754ee32ebb535ea2a0b0c16ef655303443
Deepak <deepakkumarlamoria@gmail.com>
2024-02-21 20:46:47 +0530

    Day 11:  Sleep

--- LeetCode.js
+++ LeetCode.js
@@ -343,3 +343,21 @@ var missingNumber = function(nums) {
 };
 
 
+//----------------------------------------------------
+
+/**
+ * Day 11: 2621. Sleep
+ * 
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    await new Promise(resolve => setTimeout(resolve, millis));
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
+
+

c8c7c5907d0e2c1cea0162ac33009b1da0313a51
Deepak <deepakkumarlamoria@gmail.com>
2024-02-27 20:31:40 +0530

    Day 14 :  Array Wrapper

--- LeetCode.js
+++ LeetCode.js
@@ -415,3 +415,43 @@ var sortBy = function(arr, fn) {
     return arr.sort((a, b) => fn(a) - fn(b));
 };
 
+
+//----------------------------------------------------------------
+
+/**
+ * 
+ * Day 14 : 2695. Array Wrapper
+ * 
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+    this.nums = nums;
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    let sum = 0;
+    for(let i=0; i<this.nums.length;i++){
+        sum = sum+this.nums[i];
+    }
+    return sum;
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+    return "\["+this.nums+"\]";
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
+

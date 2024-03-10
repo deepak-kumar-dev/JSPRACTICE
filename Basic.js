05d275fad1919e4095eb85c0742eb003ab7d630e
Deepak <deepakkumarlamoria@gmail.com>
2024-03-10 09:25:05 +0530

    2413. Smallest Even Multiple

--- Basic.js
+++ Basic.js
@@ -32,3 +32,13 @@ var fizzBuzz = function(n) {
     }
     return res;
 };
+
+//-----------------------------------------------
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var smallestEvenMultiple = function(n) {
+    return n%2 === 0 ? n : n*2;
+};

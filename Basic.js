bd7d2af8c29603a50a261787c09f7442ba834478
Deepak <deepakkumarlamoria@gmail.com>
2024-03-14 20:10:25 +0530

    1281. Subtract the Product and Sum of Digits of an Integer

--- Basic.js
+++ Basic.js
@@ -170,3 +170,21 @@ var differenceOfSums = function(n, m) {
     }
     return nd - d;
 };
+
+
+//------------------------------------------
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var subtractProductAndSum = function(n) {
+    let sum = 0;
+    let mul = 1;
+    while(n > 0){
+        sum = sum + (n%10);
+        mul = mul * (n%10);
+        n = Math.floor(n/10, 0);
+    }
+    return mul - sum;
+};

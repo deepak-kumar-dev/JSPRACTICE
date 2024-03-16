4aff0da577bf29a05139568fd092d487890a1902
Deepak <deepakkumarlamoria@gmail.com>
2024-03-16 21:27:36 +0530

    507. Perfect Number

--- Basic.js
+++ Basic.js
@@ -251,3 +251,20 @@ var distributeCandies = function (candies, num_people) {
     }
     return array;
 };
+
+//=====================================
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber = function (num) {
+    if (num % 2 != 0) return false;
+    let sum = 0;
+    for (let i = 1; i <= num / 2; i++) {
+        if (num % i == 0) {
+            sum = sum + i;
+        }
+    }
+    return num == sum ? true : false;
+};

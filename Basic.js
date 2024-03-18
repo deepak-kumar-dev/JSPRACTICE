b9d5126b54965a7e31985e03fedb1a1d67ac45f9
Deepak <deepakkumarlamoria@gmail.com>
2024-03-18 20:23:42 +0530

    2119. A Number After a Double Reversal

--- Basic.js
+++ Basic.js
@@ -268,3 +268,15 @@ var checkPerfectNumber = function (num) {
     }
     return num == sum ? true : false;
 };
+
+//------------------------------------
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isSameAfterReversals = function (num) {
+    if (num == 0) return true;
+    if (num % 10 == 0) return false;
+    return true;
+};
